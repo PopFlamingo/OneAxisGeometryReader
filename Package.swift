550fe4ecf582cde863b772855f8b447b6edef2cf
Benjamin Lea <benjamin@canva.com>
2021-05-08 16:57:23 +1000

    Fix macOS minimum version

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ import PackageDescription
 let package = Package(
     name: "OneAxisGeometryReader",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13), .watchOS(.v6), .tvOS(.v13)
+        .macOS(.v10_15), .iOS(.v13), .watchOS(.v6), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
