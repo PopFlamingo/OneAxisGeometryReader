730e67fcbff25e6d7c71980c3302d0d2612b37d5
adtrevor <trevor.annedenise@icloud.com>
2021-07-23 22:51:55 +0200

    Deprecate Horizontal/VerticalGeometryReader in favor of HGeometryReader and VGeometryReader

---
+++ Sources/OneAxisGeometryReader/Deprecated.swift
@@ -0,0 +1,5 @@
+@available(*, deprecated, renamed: "HGeometryReader")
+public typealias HorizontalGeometryReader = HGeometryReader
+
+@available(*, deprecated, renamed: "VGeometryReader")
+public typealias VerticalGeometryReader = VGeometryReader
